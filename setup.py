cb52cfa5ec4c9bc4c105a0c899c51f528d137a33
eytan <edaniyalzade@walmartlabs.com>
2018-07-30 09:12:45 -0400

    make long description content type markdown

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=['django_reverse_admin',],
     license='BSD',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     classifiers=[
         "License :: OSI Approved :: BSD License",
     ]
