3f9fc857b5b93c4f388f6834eb59102e55699f74
eytan <edaniyalzade@walmartlabs.com>
2018-08-06 09:54:19 -0400

    add a sample application for testing

---
+++ tests/polls/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.db import models
+from polls.models import Address
+from polls.models import Person
+from django_reverse_admin import ReverseModelAdmin
+
+class PersonAdmin(ReverseModelAdmin):
+    inline_type = 'tabular'
+    inline_reverse = [('home_addr', {'fields': ['street', 'city', 'state', 'zipcode']}),
+                      ]
+admin.site.register(Person, PersonAdmin)
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('street', 'zipcode', 'city', 'state', '_person')
+
+admin.site.register(Address, AddressAdmin)
