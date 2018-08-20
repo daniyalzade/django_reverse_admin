7a2bbffcc90c34c2ad098d77c64418c023874b23
eytan <edaniyalzade@walmartlabs.com>
2018-08-19 21:01:46 -0400

    add street 2 for testing blank / nullable values

--- tests/polls/models.py
+++ tests/polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
+    street_2 = models.CharField(max_length=255, blank=True, null=True)
     zipcode = models.CharField(max_length=10)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
@@ -10,11 +11,16 @@ class Address(models.Model):
         return self.person
 
     def __str__(self):
-        return '{}, {}, {}'.format(self.street, self.city, self.zipcode)
+        street_2 = ''
+        if self.street_2:
+            street_2 = ' - {}'.format(self.street_2)
+        return '{}{}, {}, {}'.format(self.street, street_2, self.city, self.zipcode)
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
     home_addr = models.OneToOneField(Address,
+                                     blank=True,
+                                     null=True,
                                      related_name='person',
                                      on_delete=models.CASCADE
                                      )
