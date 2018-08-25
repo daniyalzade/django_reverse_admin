4599ba5413f2e17694f35d459015a6920058a40c
eytan <edaniyalzade@walmartlabs.com>
2018-08-24 23:26:16 -0700

    put the testing db in the test folder. probably this is not hte right thing to do

--- tests/settings.py
+++ tests/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'tests/db.sqlite3'),
     }
 }
 
