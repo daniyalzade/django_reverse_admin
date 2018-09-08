690e01de5a24c14ec01055cec71a68ff03b4f94d
eytan <edaniyalzade@walmartlabs.com>
2018-09-08 13:44:33 -0400

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django_reverse_admin',
-    version='1.0.0',
+    version='1.1.0',
     packages=['django_reverse_admin',],
     license='BSD',
     long_description=open('README.md').read(),
