b23c8bf5a8afca68af3abcec33b2a3b7aa148c3f
eytan <eytan@bozburunlabs.com>
2020-07-04 15:57:22 -0400

    Bump version: 2.8.9 → 2.8.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django_reverse_admin',
-    version='2.8.9',
+    version='2.8.10',
     packages=['django_reverse_admin',],
     license='BSD',
     long_description=open('README.md').read(),
